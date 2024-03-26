edc2f30b49655c97ab578525ba8266e3f8fb5573
Mayara <mayara.wojceikovski@gmail.com>
2024-03-26 16:22:37 -0300

    exercicio18

--- programa.java
+++ programa.java
@@ -17,8 +17,8 @@ public class Programa {
          //exercicio14.executar();
        //  exercicio15.executar();
         // exercicio16.executar();
-         exercicio17.executar();
-        //exercicio18.executar();
+        // exercicio17.executar();
+        exercicio18.executar();
         // exercicio19.executar();
         // exercicio20.executar();
         // exercicio21.executar();
