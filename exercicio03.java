51c3644be5deb23e9a10521f2ac11f9ac883c358
Mayara <mayara.wojceikovski@gmail.com>
2024-03-25 23:38:51 -0300

    vendedor

---
+++ exercicio03.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exercicio03 {
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite o nome do vendedor:");
+    String nome = scanner.nextLine();
+
+    System.out.println("Digite o salário fixo do vendedor:");
+    double salarioFixo = scanner.nextDouble();
+
+    System.out.println("Digite o total de vendas do vendedor:");
+    double totalVendas = scanner.nextDouble();
+
+    Vendedor vendedor = new Vendedor(nome, salarioFixo, totalVendas);
+
+    vendedor.exibirInformacoes();
+
+    scanner.close();
+}
+}
---
+++ programa3.java
@@ -0,0 +1,5 @@
+public class programa3 {
+    public static void main(String[] args) {
+        exercicio03.main(args);
+    }
+}
